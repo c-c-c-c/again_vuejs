5697927688b50520c37f8bcec518fa32ee711798
4c <4c.mura@gmail.com>
2018-02-21 22:16:12 +0900

    条件追加機能実装

--- public/assets/script.js
+++ public/assets/script.js
@@ -6,11 +6,20 @@
 		template: '#term-component'
 	});
 
+	var count = 6; //本当は、初期値は下のtermsの数にした方が良い
 	var vm = new Vue({
 		el: '#app',
+
 		data: {
 			terms: [{ id: 1, content: "term1" }, { id: 2, content: "term2" }, { id: 3, content: "term3" }, { id: 4, content: "term4" }, { id: 5, content: "term5" }, { id: 6, content: "term6" }]
+		},
+		methods: {
+			addTerm: function addTerm() {
+				count++;
+				this.terms.push({ id: count, content: count });
+			}
 		}
+
 	});
 })();
 //# sourceMappingURL=script.js.map
--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,28 @@
 (function() {
 
-
 	Vue.component('termComponent', {
 		template: '#term-component'
 	});
 
+	let count = 6;//本当は、初期値は下のtermsの数にした方が良い
 	var vm = new Vue({
 		el: '#app',
+
 		data: {
 			terms:[
 				{id:1, content:"term1" },{id:2, content:"term2"},{id:3, content:"term3"},
 				{id:4, content:"term4"},{id:5, content:"term5"},{id:6, content:"term6"}
 		 ]
+	 },
+		methods: {
+			addTerm: function() {
+				count++;
+				this.terms.push(
+					{id:count, content:count}
+				);
+
+			}
 		}
+
 	});
 })();
