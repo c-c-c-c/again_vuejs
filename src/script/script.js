384b9e13edb40a1064d2edcea204746f207990fe
4c <4c.mura@gmail.com>
2018-02-21 16:50:44 +0900

    2種類のコンポーネント

--- src/script/script.js
+++ src/script/script.js
@@ -1,10 +1,7 @@
-
 (function() {
-
 	// Vue.component('my-component', {
 	// 	template: '<div>A custom component!</div>'
 	// })
-
 	// var Child = {
 	// 	template: '<div>カスタムコンポーネント</div>'
 	// }
@@ -12,16 +9,24 @@
 	// 		template:"#test_component_id",
 	// });
 
-	var testComponent = Vue.extend({
-		template:'<button>Ozaki Love</button>'
+	// var testComponent = Vue.extend({
+	// 	template:'<button>Ozaki Love</button>'
+	// });
+
+	Vue.component('testComponent', {
+  	template: '#test-component'
+	});
+	Vue.component('testComponent2', {
+		template: '#test-component2'
 	});
 
 	var vm = new Vue({
 		el: '#app',
 		data: {
-			name:'JS内にコンポーネント定義'
+			name:'JS内に2種類コンポ定義'
 		},
-		components: { 'test-component': testComponent },
+		// components: { 'test-component': testComponent },
+		// components: { 'test-component2': testComponent2 },
 		// template:'<test-component>'
 
 	});
