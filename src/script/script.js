1309873f92242f16f8ede4bdb39655ba2bc37fe7
4c <4c.mura@gmail.com>
2018-02-21 21:35:23 +0900

    葛藤の跡を残して一度コミット

--- src/script/script.js
+++ src/script/script.js
@@ -22,13 +22,20 @@
 
 	Vue.component('termComponent', {
 		template: '#term-component'
+				// props:['term-id']
+
 	});
 
 	var vm = new Vue({
 		el: '#app',
 		data: {
-			name:'JS内にコンポーネント定義'
-		},
+			// name:'JS内にコンポーネント定義',
+			// terms:['term1','term2','term3','term4','term5', 'term6']
+			terms:[
+				{id:1, content:"term1" },{id:2, content:"term2"},{id:3, content:"term3"},
+				{id:4, content:"term4"},{id:5, content:"term5"},{id:6, content:"term6"}
+		 ]
+		}
 		// components: { 'test-component': testComponent },
 		// components: { 'test-component2': testComponent2 },
 		// template:'<test-component>'
