730b39f07dad4af8f25c764739095e76ac818c94
4c <4c.mura@gmail.com>
2018-02-22 14:58:43 +0900

    削除機能完了

--- public/assets/script.js
+++ public/assets/script.js
@@ -3,13 +3,17 @@
 (function () {
 
 	Vue.component('termComponent', {
-		template: '#term-component'
+		template: '#term-component',
+		methods: {
+			deleteTerm: function deleteTerm() {
+				this.$emit('delete-term', this._uid - 1); //なぜかindexがうまく取れない
+			}
+		}
 	});
 
 	var count = 6; //本当は、初期値は下のtermsの数にした方が良い
 	var vm = new Vue({
 		el: '#app',
-
 		data: {
 			terms: [{ id: 1, content: "term1" }, { id: 2, content: "term2" }, { id: 3, content: "term3" }, { id: 4, content: "term4" }, { id: 5, content: "term5" }, { id: 6, content: "term6" }]
 		},
@@ -18,7 +22,8 @@
 				count++;
 				this.terms.push({ id: count, content: count });
 			},
-			deleteTerm: function deleteTerm(index) {
+			// deleteTermEmit: function(index) {
+			deleteTermEmit: function deleteTermEmit(index) {
 				if (confirm("この条件をほんとうに消しますか")) {
 					this.terms.splice(index, 1);
 				}
--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,23 @@
 (function() {
 
 	Vue.component('termComponent', {
-		template: '#term-component'
+		template: '#term-component',
+		methods: {
+			deleteTerm: function() {
+				this.$emit('delete-term',(this._uid - 1));//なぜかindexがうまく取れない
+			}
+		}
 	});
 
 	let count = 6;//本当は、初期値は下のtermsの数にした方が良い
 	var vm = new Vue({
 		el: '#app',
-
 		data: {
 			terms:[
 				{id:1, content:"term1" },{id:2, content:"term2"},{id:3, content:"term3"},
 				{id:4, content:"term4"},{id:5, content:"term5"},{id:6, content:"term6"}
-		 ]
-	 },
+		 	]
+	 	},
 		methods: {
 			addTerm: function() {
 				count++;
@@ -21,7 +25,8 @@
 					{id:count, content:count}
 				);
 			},
-			deleteTerm: function(index) {
+			// deleteTermEmit: function(index) {
+			deleteTermEmit: function(index) {
 				if(confirm("この条件をほんとうに消しますか")) {
 					this.terms.splice(index,1)
 				}
